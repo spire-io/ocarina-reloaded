febc94abd8c87ce46965c7c9f3dbd2b6b5a78efc
Nicolas LaCasse <nlacasse@borderstylo.com>
2012-06-18 12:03:32 -0700

    delete unused code

--- javascript/initialScript.js
+++ javascript/initialScript.js
@@ -3,16 +3,6 @@ $(document).ready(function(){
   spire = new Spire();
 
   spire.start("YOUR-ACCOUNT-SECRET", function (err) {
-    if (!err) {
-      // You now have a spire session.
-      // Start creating channels and subscripions.
-      
-      spire.session.applications$(function (err, applications) {
-        if (!err) {
-         
-        }
-      });
-      
       // Create our app
       // You can name the application anything you like
       spire.session.createApplication("ocarinaReloaded", function (err, app) {
@@ -53,25 +43,6 @@ function createChannels (app) {
       return;
     }
   }); //create_channel
-         
-  var channelName = "ocarinaChatChannel";
-  app.createChannel(channelName, function (err, chatChannel) {
-    if (!err){
-      // Create a subscription for the newly created channel so our users can read messages
-      var subscriptionName = 'ocarinaChatSubscription'
-      chatChannel.subscription(subscriptionName, function (err, gameSubscription) {
-        if (!err) {
-          // `subscription` is the new named subscription.
-        } else {
-          $('h1').append('A subscription named ' + subscriptionName + 'already exists');
-          return;
-        }
-      }); //subscription
-    } else {
-      $('h1').append('A channel named ' + channelName + 'already exists');
-      return;
-    }
-  }); //create_channel
-  
+
   $('h1').append('Done! Ready to go');
 }
